bee8435c5bc238e56eb6425c3cc4a1e0942f0eeb
Guan Wu <beautytanned@gmail.com>
2022-08-18 15:29:19 +0800

    Create tag table. Insert Author and Tag info into database. Download author image.

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Storage;
 
 class StoryController extends Controller
 {
@@ -19,51 +21,40 @@ class StoryController extends Controller
 			'csv_file' => ['required', 'file', 'mimes:csv'],
 		]);
 
-		if($request->csv_file !== null){
+		if ($request->csv_file !== null) {
 			//save csv file to public folder
 			$csvFileName = $request->csv_file->getClientOriginalName();
-			$request->csv_file->move(public_path('/'),$csvFileName);
+			$request->csv_file->move(public_path('/'), $csvFileName);
 
-			$csv = $_SERVER['DOCUMENT_ROOT'].'/royalroad-stories.csv';
+			$csv = $_SERVER['DOCUMENT_ROOT'] . '/royalroad-stories.csv';
 
 			$row = 1;
 			if (($handle = fopen($csv, "r")) !== FALSE) {
-				while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+				while (($data = fgetcsv($handle, 1000000, ",")) !== FALSE) {
 
 
-					//get author name
-					if(isset($data[20])){
-						$authorName = $data[20];
-					}
+					$num = count($data);
 
-
-
-					//get author image src
-					if(isset($data[21])){
-						$authorImage = $data[21];
-					}
-
-					if($authorName !== ""){
-						$authorInfo = Author::where('name','=',$authorName)->first();
-						if(!$authorInfo){
-							Author::create([
-								'name' => $authorName,
-								'image' => $authorImage
-							]);
-
-							echo "Author : ".$authorName." added.<br>";
+					if ($num !== 22) {
+						//check if csv is broken
+						for ($r = 0; $r < $num; $r++) {
+							echo $r . '=>' . $data[$r] . '<br>';
 						}
+					} else {
+						if ($row == 1) {
+//							for ($r = 0; $r < $num; $r++) {
+//								echo $r . '=>' . $data[$r] . '<br>';
+//							}
+						} else {
+//							$this->saveAuthor($data[20], $data[21]);
+//
+//							$this->saveTags($data[4]);
+
+						}
+
 					}
 
-
-
-
-//					$num = count($data);
-//					echo "<p style='color: red'> $num fields in line $row: <br /></p>\n";
-//					$row++;
-//					for ($r=0; $r < $num; $r++) {
-//						echo $r." => ".$data[$r] . "<br />\n";
-//					}
+					$row++;
 				}
 
 				fclose($handle);
@@ -73,17 +64,55 @@ class StoryController extends Controller
 		}
 
 
-
-
-
-
-
-
-
-
-
 //
 //		return redirect('/insertCategory');
+	}
+
+	private function saveAuthor($authorName, $authorImage)
+	{
+		echo "Author : " . $authorName . " => " . $authorImage . "<br>";
+
+		if ($authorName !== "") {
+			$authorInfo = Author::where('name', '=', $authorName)->first();
+			if (!$authorInfo) {
+
+				//download author image
+				$info = pathinfo($authorImage);
+				$imageName = $info['filename'].'.png';
+				if($authorImage !== "/dist/img/anon.jpg"){
+					$contents = file_get_contents($authorImage);
+					Storage::put('authorImages/'.$imageName, $contents);
+				}
+
+				//save author into database
+				Author::create([
+					'name' => $authorName,
+					'image' => $imageName
+				]);
+
+				echo "Author : " . $authorName . " added.<br>";
+			}
+		}
+	}
+
+	private function saveTags($story_tags)
+	{
+
+		//get story tags
+		preg_match('/"story_tags":"(.*)"/', $story_tags, $tags);
+		$tagsArr = explode('\n',$tags[1]);
+		foreach ($tagsArr as $tag){
+			$tagName = trim($tag);
+			if($tagName !== ""){
+				$tagInfo = Tag::where('tagName', '=', $tagName)->first();
+				if (!$tagInfo) {
+					Tag::create([
+						'tagName' => $tagName
+					]);
+					echo "Tag : " . $tag . " added.<br>";
+				}
+			}
+		}
 	}
 
 }
